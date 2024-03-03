34c1ef441f578c96f8cd807b9486973c598caf04
Anand <anand.sk1324@gmail.com>
2024-03-04 00:18:54 +0530

    finalCommit2

--- client/main.js
+++ client/main.js
@@ -18,13 +18,14 @@ serviceBlocks.forEach((serviceBlock) => {
 
 
 const form = document.querySelector('form');
-form.addEventListener('submit',async (event) =>{
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
     console.log(1)
 
     const Name = document.getElementById("Name");
     const Phone = document.getElementById("Phone");
     const Email = document.getElementById("Email");
+    const Msg = document.getElementById("Msg");
 
     const baseUrl = "https://dreamy-wedding-api.onrender.com/interests/"
     const posting = await fetch(baseUrl, {
@@ -34,12 +35,13 @@ form.addEventListener('submit',async (event) =>{
         },
         body: JSON.stringify({
             name: Name.value,
-            phone : Phone.value,
-            email: Email.value
+            phone: Phone.value,
+            email: Email.value,
+            msg: Msg.value
         })
     })
     console.log(await posting.json())
-    if(posting.ok == true){
+    if (posting.ok == true) {
         alert("Your response recorded succesfully")
     }
 })
